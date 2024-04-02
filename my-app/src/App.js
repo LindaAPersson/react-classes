630beb0cb6856476ce88c0275cd49c3224a8e9fa
Linda Persson <lindapersson-@live.se>
2024-04-02 07:57:05 +0000

    callbacks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import FunctionalGreeting from "./components/FunctionalGreeting";
-import FunctionalGreetingWithProps from "./components/FunctionalGreetingWithProps"
+import FunctionalGreetingWithProps from "./components/FunctionalGreetingWithProps";
+import StatefullGreetiing from "./components/StatefulGretting";
+import StatefullGreetiingWithcallback from "./components/StaefulGreetingWithCallback";
 
 function App() {
   return (
     <div className="App">
       <FunctionalGreeting/>
       <FunctionalGreetingWithProps greeting="Nice to see you" name="Linda" age="30"/>
+      <StatefullGreetiing greeting="Test" namn="Linda"/>
+      <StatefullGreetiingWithcallback />
     </div>
   );
 }
