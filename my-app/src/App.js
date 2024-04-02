f859740f4fb961503f66d0aa69a42350e5f2a255
Linda Persson <lindapersson-@live.se>
2024-04-02 11:09:44 +0000

    child

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,12 @@ import FunctionalGreeting from "./components/FunctionalGreeting";
 import FunctionalGreetingWithProps from "./components/FunctionalGreetingWithProps";
 import StatefullGreetiing from "./components/StatefulGretting";
 import StatefullGreetiingWithcallback from "./components/StaefulGreetingWithCallback";
+import EventsClass from "./components/EventsClass";
+import EventsFunctional from "./components/EventsFunctional";
+import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
+import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
+import NestingComponents from "./components/NestingComponents";
+import MethodsAsPropsParent from "./components/MethodsAsPropsParent";
 
 function App() {
   return (
@@ -12,6 +18,12 @@ function App() {
       <FunctionalGreetingWithProps greeting="Nice to see you" name="Linda" age="30"/>
       <StatefullGreetiing greeting="Test" namn="Linda"/>
       <StatefullGreetiingWithcallback />
+      <EventsClass/>
+      <EventsFunctional/>
+      <ConditionalRenderingClass/>
+      <ConditionalRenderingFunctional connected={false}/>
+      <NestingComponents/>
+      <MethodsAsPropsParent/>
     </div>
   );
 }
---
+++ my-app/src/components/EventsFunctional.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function EventsFunctional() {
+
+    function clickHandler() {
+
+    }
+    
+    return (
+        <div>
+            <button onClick= {clickHandler}>click me - functional component</button>
+        </div>
+    )
+}
+
+export default EventsFunctional
